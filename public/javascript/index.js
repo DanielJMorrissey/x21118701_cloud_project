8c603e9536d988f8b59eafc9c0b8d741871fe82d
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-03-27 19:09:52 +0100

    added js validation

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init(){
   document.addEventListener("submit", validate);
+  document.addEventListener("change", membersCheck);
 }
 
 function validate(e){
@@ -9,7 +10,16 @@ function validate(e){
   var genre = document.getElementById("band_genre").value;
   var flash = document.getElementById("flash");
   if(name.length == 0 || genre.length == 0){
-    e.preventDefault()
-    flash.innerHTML = "Please enter a name or genre!"
+    e.preventDefault();
+    flash.innerHTML = "Please enter a name or genre!";
+  }
+}
+
+function membersCheck(){
+  var members = document.getElementById("band_members");
+  if(members.value < 0){
+    members.value = members.value * -1;
+  } else if(members.value == 0){
+    members.value = 1;
   }
 }

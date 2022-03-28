42cdfcd19b6c40cb06b47c8b434e55d82a3dce08
DanielJMorrissey <danieljohnmorrissey@hotmail.com>
2022-03-28 14:56:43 +0100

    added some more js validation, some filler and started my report

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,6 +3,10 @@ window.addEventListener("DOMContentLoaded", init);
 function init(){
   document.addEventListener("submit", validate);
   document.addEventListener("change", membersCheck);
+  var name = document.getElementById("band_name");
+  name.addEventListener("change", check);
+  var genre = document.getElementById("band_genre");
+  genre.addEventListener("change", check);
 }
 
 function validate(e){
@@ -21,5 +25,19 @@ function membersCheck(){
     members.value = members.value * -1;
   } else if(members.value == 0){
     members.value = 1;
+  }
+}
+
+function check(e){
+  var field = document.getElementById(e.target.id).value;
+  if(e.target.id == "band_name"){
+    var error = document.getElementById("nameError");
+  } else if (e.target.id == "band_genre"){
+    var error = document.getElementById("genreError");
+  }
+  if(field.length == 0){
+    error.innerHTML = "Please enter a name or genre";
+  } else {
+    error.innerHTML = "";
   }
 }
